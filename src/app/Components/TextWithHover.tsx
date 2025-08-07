498fa55b6fe9a6f6be70a491ad4ca9fa6202ab42
KishoreRajesh <kishorerajesh007@gmail.com>
2025-08-07 13:42:10 +0530

    commit-1.1

--- src/app/Components/TextWithHover.tsx
+++ src/app/Components/TextWithHover.tsx
@@ -3,7 +3,7 @@ import HoverScaleWithFloatingImage from './ui/textwithimage';
 const TextwithImage = () => {
   return (
     <HoverScaleWithFloatingImage
-      text="The Airsafa Story: From Concept to Reality which will be very helpful for you to know about Airsafa"
+      text="The Airsaf Story: From Concept to Reality which will be very helpful for you to know about Airsafa"
       imageSrc="/next.svg"
       imageAlt="Floating Image"
     />
